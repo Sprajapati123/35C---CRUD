5483679d762df86798ba1a871064f0a9ecc1cac4
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-09 13:52:45 +0545

    ciewmodel

--- app/src/main/java/com/example/c35c_crud/repository/UserRepository.kt
+++ app/src/main/java/com/example/c35c_crud/repository/UserRepository.kt
@@ -10,14 +10,16 @@ interface UserRepository {
 //     "message" : "Register success"
 //    }
 
-    fun login(email: String, password: String,
-              callback: (Boolean, String) -> Unit)
 
 //    {
 //    "success" : true
 //     "message" : "Register success"
 //     "userId" : "1234"
 //    }
+
+    fun login(email: String, password: String,
+              callback: (Boolean, String) -> Unit)
+
 
     fun signup(email: String, password: String,
                callback: (Boolean, String, String) -> Unit)
---
+++ app/src/main/java/com/example/c35c_crud/viewmodel/UserViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.c35c_crud.viewmodel
+
+import com.example.c35c_crud.model.UserModel
+import com.example.c35c_crud.repository.UserRepository
+import com.google.firebase.auth.FirebaseUser
+
+class UserViewModel(var repo : UserRepository) {
+
+    fun login(email: String, password: String,
+              callback: (Boolean, String) -> Unit){
+        repo.login(email, password, callback)
+    }
+
+
+    fun signup(email: String, password: String,
+               callback: (Boolean, String, String) -> Unit){
+        repo.signup(email, password, callback)
+    }
+
+    fun addUserToDatabase(
+        userId: String, userModel: UserModel,
+        callback: (Boolean, String) -> Unit
+    ){
+        repo.addUserToDatabase(userId, userModel, callback)
+    }
+
+    fun forgetPassword(email:String,
+                       callback: (Boolean, String) -> Unit){
+        repo.forgetPassword(email, callback)
+    }
+
+    fun getCurrentUser() : FirebaseUser?{
+        return repo.getCurrentUser()
+    }
+
+}
