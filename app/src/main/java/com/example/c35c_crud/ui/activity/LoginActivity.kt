eceae22359e93ea12ae7adcec7df845aa2c24477
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-09 14:31:05 +0545

    navigatio

--- app/src/main/java/com/example/c35c_crud/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/c35c_crud/ui/activity/LoginActivity.kt
@@ -2,24 +2,51 @@ package com.example.c35c_crud.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.c35c_crud.R
 import com.example.c35c_crud.databinding.ActivityLoginBinding
+import com.example.c35c_crud.repository.UserRepositoryImpl
+import com.example.c35c_crud.viewmodel.UserViewModel
 
 class LoginActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityLoginBinding
+    lateinit var userViewModel: UserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var repo = UserRepositoryImpl()
+        userViewModel = UserViewModel(repo)
+
+        binding.btnLogin.setOnClickListener {
+            var email = binding.editEmail.text.toString()
+            var password = binding.editPassword.text.toString()
+
+            userViewModel.login(email, password) { success, message ->
+                if (success) {
+                    var intent = Intent(this@LoginActivity,
+                        NavigationActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@LoginActivity,
+                        message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
         binding.btnSignupnavigate.setOnClickListener {
-            val intent = Intent(this@LoginActivity,
-                RegisterActivity::class.java)
+            val intent = Intent(
+                this@LoginActivity,
+                RegisterActivity::class.java
+            )
             startActivity(intent)
         }
 
