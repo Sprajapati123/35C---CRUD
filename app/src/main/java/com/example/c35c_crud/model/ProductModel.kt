32a35aaeed102d07943e1d957b37a3e3895d67f3
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-15 14:11:43 +0545

    CRUD

---
+++ app/src/main/java/com/example/c35c_crud/model/ProductModel.kt
@@ -0,0 +1,9 @@
+package com.example.c35c_crud.model
+
+data class ProductModel(
+    var productId : String = "",
+    var productName : String = "",
+    var productDesc : String = "",
+    var price : Int = 0,
+) {
+}
