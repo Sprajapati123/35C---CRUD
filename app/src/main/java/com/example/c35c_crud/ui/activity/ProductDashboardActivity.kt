443baa8b230a8c7e23d2ed63a61ec82ae0690b31
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-16 14:01:43 +0545

    add product

---
+++ app/src/main/java/com/example/c35c_crud/ui/activity/ProductDashboardActivity.kt
@@ -0,0 +1,32 @@
+package com.example.c35c_crud.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.c35c_crud.R
+import com.example.c35c_crud.databinding.ActivityProductDashboardBinding
+
+class ProductDashboardActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProductDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityProductDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener {
+            var intent = Intent(this@ProductDashboardActivity,
+                AddProductActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
