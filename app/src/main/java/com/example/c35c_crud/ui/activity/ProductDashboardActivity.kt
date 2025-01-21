f4b92c57f9ade3bf2e5aaa33727eaa9d8fa57dfb
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-21 12:20:31 +0545

    Retreieve

--- app/src/main/java/com/example/c35c_crud/ui/activity/ProductDashboardActivity.kt
+++ app/src/main/java/com/example/c35c_crud/ui/activity/ProductDashboardActivity.kt
@@ -2,21 +2,58 @@ package com.example.c35c_crud.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.c35c_crud.R
+import com.example.c35c_crud.adapter.ProductAdapter
 import com.example.c35c_crud.databinding.ActivityProductDashboardBinding
+import com.example.c35c_crud.repository.ProductRepositoryImpl
+import com.example.c35c_crud.viewmodel.ProductViewModel
+import java.util.ArrayList
 
 class ProductDashboardActivity : AppCompatActivity() {
     lateinit var binding: ActivityProductDashboardBinding
+
+    lateinit var productViewModel: ProductViewModel
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityProductDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var repo = ProductRepositoryImpl()
+        productViewModel = ProductViewModel(repo)
+
+        adapter = ProductAdapter(this@ProductDashboardActivity,
+            ArrayList())
+
+        productViewModel.getAllProducts()
+
+        productViewModel.allProducts.observe(this){products->
+            products?.let {
+                adapter.updateData(it)
+            }
+        }
+
+        productViewModel.loading.observe(this){loading->
+            if(loading){
+                binding.progressBar.visibility = View.VISIBLE
+            }else{
+                binding.progressBar.visibility = View.GONE
+
+            }
+        }
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
 
         binding.floatingActionButton.setOnClickListener {
             var intent = Intent(this@ProductDashboardActivity,
